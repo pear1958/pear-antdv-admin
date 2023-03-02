9020ba8f2f400724062eaa0e97ff10b603c3e78e
zhangzy56 <doxoops@163.com>
2023-03-02 11:03:49 +0800

    fix: 添加函数

--- src/utils/common.js
+++ src/utils/common.js
@@ -45,3 +45,19 @@ export function deepClone(obj, hash = new WeakMap()) {
 
   return cloneObj
 }
+
+export function copyText(text) {
+  // text是复制文本
+  // 创建input元素
+  const el = document.createElement('input')
+  // 给input元素赋值需要复制的文本
+  el.setAttribute('value', text)
+  // 将input元素插入页面
+  document.body.appendChild(el)
+  // 选中input元素的文本
+  el.select()
+  // 复制内容到剪贴板
+  document.execCommand('copy')
+  // 删除input元素
+  document.body.removeChild(el)
+}
