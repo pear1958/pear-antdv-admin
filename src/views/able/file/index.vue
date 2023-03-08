bcea203a82da7072e70ce7b2e11b300257de50b5
zhangzy56 <doxoops@163.com>
2023-03-08 17:22:01 +0800

    fix: 修改Bug

--- src/views/able/file/index.vue
+++ src/views/able/file/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <a-button type="primary" @click="downloadPdf">点击下载 Pdf</a-button>
+    <a-button type="primary" @click="downloadPdf">点击下载 在线图片</a-button>
+    <a-button type="primary" @click="downloadPdf">点击下载 在线xlsx</a-button>
   </div>
 </template>
 
@@ -8,15 +10,18 @@
 import axios from 'axios'
 import { downloadByData } from '@/hooks/useDownload'
 
+// PDF不能使用在线url直接下载, 需要接口返回流数据才能下载
 function downloadPdf() {
+  const fileUrl = 'http://cdn.xxxxx.cn/saas/uploads/202303/6408378b10d45.pdf'
+
   // axios({
   //   method: 'GET',
-  //   url: this.fileUrl,
+  //   url: fileUrl,
   //   responseType: 'blob',
   //   withCredentials: false
   // })
   //   .then(res => {
-  //     const name = this.fileUrl
+  //     const name = fileUrl
   //     const suffix = name.substring(name.lastIndexOf('.') + 1)
   //     const fileName = `temp.${suffix}`
 
